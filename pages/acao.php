52a6f81fb4ecd4f7bcf1310b1aac1afef8da7b0e
PEDRO GONCALVES FARINHAS <pedro.gfarinhas@senacsp.edu.br>
2025-09-29 16:13:54 -0300

    tudo funcionando menos a lista das fronteiras

--- pages/acao.php
+++ pages/acao.php
@@ -2,40 +2,40 @@
 
 $selecao = "";
 
-if (!isset($_GET['pais'] ))
-    {
-        echo"selecione";
-      
+if (!isset($_GET['pais'])) {
+        echo "selecione";
 } else {
 
-     $selecao = $_GET['pais'];
+        $selecao = $_GET['pais'];
 
-      // var_dump($selecao);
+        // var_dump($selecao);
 
-    
-     $url = "https://restcountries.com/v3.1/name/{$selecao}";
 
-      $configuracoes = [
+        $url = "https://restcountries.com/v3.1/name/{$selecao}";
+
+        $configuracoes = [
                 "http" => [
                         "method" => "GET",
                         "header" => "Content-Type: appllication/json"
                 ]
         ];
-         $context = stream_context_create($configuracoes); 
-         $response = file_get_contents($url, false, $context);
+        $context = stream_context_create($configuracoes);
+        $response = file_get_contents($url, false, $context);
 
-         // var_dump($selecao);
+        // var_dump($selecao);
         //  var_dump($response);
-         if ($response == false) {
+        if ($response == false) {
                 $mensagem = "Erro ao acessar API countries";
         } else {
 
-                $dados = json_decode($response, true);  
-                if (isset($dados['not found']) == true) { 
-                        $mensagem = "dados não encontrados";}
+                $dados = json_decode($response, true);
+                if (isset($dados['not found']) == true) {
+                        $mensagem = "dados não encontrados";
                 }
-    
-  //var_dump($dados);
+        }
+        // pre é uma tag html que organiza o vardump
+        //var_dump($dados);
+        //selection e option
 }
 
 
@@ -44,23 +44,39 @@ if (!isset($_GET['pais'] ))
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
         <meta charset="UTF-8">
         <meta commom="viewport" content="width=device-width, initial-scale=1.0">
         <title>Resultado </title>
 </head>
-<body>
-        <div >
-               
-                        <label> Extenção do país </label> <br>
-                        <input type="text" value="<?= isset( $dados [0]['tld'][0]) ? $dados[0]['tld'][0]:"correspondência não encontrada "?>" disabled >  <br> 
 
-                        <label> Capital </label> <br>
-                        <input type="text" value="<?= isset( $dados [0]['capital'][0]) ? $dados[0]['capital'][0]:"correspondência não encontrada"?>" disabled >   <br>
-                                                
-                        <label> nome comum no mundo </label> <br>
-                        <input type="text" value="<?= isset( $dados [0]['name']['commom'][1]) ? $dados[0]['name']['commom'][1]:"correspondência não encontrada"?>" disabled >   <br>
-                                                
+<body>
+
+        <div>
+
+                <label> Extenção do país </label> <br>
+                <input type="text" value="<?= isset($dados[0]['tld'][0]) ? $dados[0]['tld'][0] : "correspondência não encontrada " ?>" disabled> <br>
+
+                <label> Capital </label> <br>
+                <input type="text" value="<?= isset($dados[0]['capital'][0]) ? $dados[0]['capital'][0] : "correspondência não encontrada" ?>" disabled> <br>
+
+                <label> nome comum no mundo </label> <br>
+                <input type="text" value="<?= isset($dados[0]['name']['common']) ? $dados[0]['name']['common'] : "correspondência não encontrada" ?>" disabled> <br>
+
+
+                <label> bordas </label> <br>
+                <input type="text" value="<?= isset($dados[0]['borders'][0]) ? $dados[0]['borders'][0] : "correspondência não encontrada" ?>" disabled> <br>
+
+
+                <label> Bandeiras </label> <br>
+                <img src="<?= isset($dados[0]['flags']['png']) ? $dados[0]['flags']['png'] : "caminho/para/imagem_padrao.png" ?>" alt="Bandeira" width="50" height="30">
+                <br>
+
+
         </div>
+        <img src="" alt="">
 </body>
+
 </html>
+
